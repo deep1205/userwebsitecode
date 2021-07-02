cc54e36e529fdb3f27b682dba815462b374f673d
Deepesh arya <deepesharya1205@gmail.com>
2021-07-02 19:56:25 +0530

    my commit

--- src/Components/TokenComponent/Token.component.js
+++ src/Components/TokenComponent/Token.component.js
@@ -1,11 +1,23 @@
 import React,{useState} from 'react'
 import axios from 'axios'
-import { Map,GoogleApiWrapper} from 'google-maps-react'
+import GoogleMapReact from "google-map-react";
 import './tokenComponent.style.css'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Track=(props)=>{
+
+
+   const defaultProps = {
+     center: {
+       lat: 22.9734229,
+lng: 78.6568942
+     },
+     zoom: 12,
+   };
+
+
+
     const [isChecked,setChecked]=useState(false)
     const [rideid,setRideId]=useState("")
     const handleRideId=(e)=>{
@@ -41,60 +53,59 @@ const Track=(props)=>{
         width:'100%',
         height:'90%'
       }
-      return(
-          <div>
-            <ToastContainer
-                position="top-center"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-            />
-            <Map
-            google={props.google}
-            zoom={8}
-            style={style}
-            disableDefaultUI={true}
-            draggable={false}
-      />
-            <div class="login-page">
-          <div class="form">
-              <div class="login-header">
-                Track Ambulance
-              </div>
-            <form onSubmit={handleSubmit}>
-              <div className="group">
-                <input
-                type="text"
-                name="tokenID"
-                value={rideid}
-                placeholder="Enter your token here" 
-                onChange={handleRideId}
-                />
-              </div>
-              <div className="group">
-              <input 
-              type='checkbox'
-              name="isChecked" 
-              checked={isChecked} 
-              onChange={handleCheckBox}
-              />
-              <label>Click Checkbox to proceed</label>
+      return (
+        <div>
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+          <div style={{ height: "100vh", width: "100%" ,position:"absolute"}}>
+            <GoogleMapReact
+              bootstrapURLKeys={{
+                key: "AIzaSyATwnp3e3ZL7__Oskpdo8Gutgls6ir4FeU",
+              }}
+              defaultCenter={defaultProps.center}
+              defaultZoom={defaultProps.zoom}
+            ></GoogleMapReact>
+          </div>
+          <div class="login-page">
+            <div class="form">
+              <div class="login-header">Track Ambulance</div>
+              <form onSubmit={handleSubmit}>
+                <div className="group">
+                  <input
+                    type="text"
+                    name="tokenID"
+                    value={rideid}
+                    placeholder="Enter your token here"
+                    onChange={handleRideId}
+                  />
+                </div>
+                <div className="group">
+                  <input
+                    type="checkbox"
+                    name="isChecked"
+                    checked={isChecked}
+                    onChange={handleCheckBox}
+                  />
+                  <label>Click Checkbox to proceed</label>
+                </div>
+                <div className="group">
+                  <button className="button" disabled={!isChecked}>
+                    Track Now
+                  </button>
+                </div>
+              </form>
             </div>
-              <div className="group">
-              <button className="button" 
-              disabled={!isChecked}>Track Now</button>
-              </div>
-            </form>
           </div>
         </div>
-        </div>
-        )
+      );
     }
-    export default GoogleApiWrapper({
-      apiKey: 'AIzaSyATwnp3e3ZL7__Oskpdo8Gutgls6ir4FeU'
-    })(Track)
+    export default Track
